4d0b6ceedaa97edd3965cfaf7542e60078932d36
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-11-22 12:05:50 +0100

    Logger and Security

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -8,16 +8,29 @@ import {
   getGroupFiles
 } from '../controllers/groupController';
 import { protect } from '../middleware/auth';
+import { cache, clearCache } from '../middleware/cache';
 
 const router = express.Router();
 
 router.use(protect);
 
-router.post('/', createGroup);
-router.get('/', getMyGroups);
-router.get('/:id', getGroup);
-router.get('/:id/files', getGroupFiles);
-router.put('/:id', updateGroup);
-router.delete('/:id', deleteGroup);
+router.post('/', async (req, res, next) => {
+  await createGroup(req as any, res);
+  await clearCache('cache:/api/groups*');
+});
+
+router.get('/', cache(300), getMyGroups);
+router.get('/:id', cache(300), getGroup);
+router.get('/:id/files', cache(300), getGroupFiles);
+
+router.put('/:id', async (req, res, next) => {
+  await updateGroup(req as any, res);
+  await clearCache('cache:/api/groups*');
+});
+
+router.delete('/:id', async (req, res, next) => {
+  await deleteGroup(req as any, res);
+  await clearCache('cache:/api/groups*');
+});
 
 export default router;
